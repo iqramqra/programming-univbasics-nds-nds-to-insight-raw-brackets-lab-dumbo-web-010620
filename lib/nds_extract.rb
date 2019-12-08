3a0f1cc5ca7c1239f7c827b79965d534371bb5a3
Iqra Masroor <masrooriqra@gmail.com>
2019-12-08 19:37:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-
 def directors_totals(nds)
+ 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
+   pp nds
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
@@ -20,3 +21,21 @@ def directors_totals(nds)
   # Be sure to return the result at the end!
   nil
 end
+
+[
+  {:name=>"Stephen Spielberg",
+    :movies=>[
+    {:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000,:release_year=>1975},
+    {:title=>"Close Encounters of the Third Kind",:studio=>"Columbia", :worldwide_gross=>135189114,:release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",:studio=>"Paramount",:worldwide_gross=>248159971,:release_year=>1981},
+    {:title=>"E.T. the Extra-terrestrial",:studio=>"Universal",:worldwide_gross=>435110554,:release_year=>1982},
+    {:title=>"Schindler's List",:studio=>"Universal",:worldwide_gross=>96898818,:release_year=>1993},
+    {:title=>"Lincoln", :studio=>"Buena Vista",:worldwide_gross=>182207973,:release_year=>2012}
+    ]
+  },
+  {:name=>"Russo Brothers",
+  :movies=>[
+    {:title=>"Avengers Endgame", :studio=>"Buena Vista", :worldwide_gross=>858371337, :release_year=>2019},
+    {:title=>"Avengers Infinity War", :studio=>"Buena Vista", :worldwide_gross=>678815482,:release_year=>2018},
+    {:title=>"Captain America Civil War",:studio=>"Buena Vista", :worldwide_gross=>408084349,:release_year=>2016},
+   
