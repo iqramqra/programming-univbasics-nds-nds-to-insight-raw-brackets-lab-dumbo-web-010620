7daabedc964528448d9f14b2b76ffa003a0d5eec
Iqra Masroor <masrooriqra@gmail.com>
2019-12-08 19:58:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,20 +20,17 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   totals= { }
-row_index =0 
-while row_index < nds.length do 
-
-director=nds [row_index][:name]
-totals[director]= 0
-column_index=0
-while column_index < nds[row_index][:movies].length do 
-
-totals[director]+=nds[row_index][:movies][column_index][:worldwide_gross]
-column_index += 1 
-end 
-row_index += 1 
-end
-
+    row_index =0 
+    while row_index < nds.length do 
+    director=nds [row_index][:name]
+    totals[director]= 0
+    column_index=0
+    while column_index < nds[row_index][:movies].length do 
+    totals[director]+=nds[row_index][:movies][column_index][:worldwide_gross]
+    column_index += 1 
+    end 
+  row_index += 1 
+  end
 totals 
   nil
 end
